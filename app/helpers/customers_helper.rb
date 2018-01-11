b8e28c64f34cc3ceeb266979cd1ad5f5ddea93d4
ccm2012 <ccm.hsss.2012@gmail.com>
2018-01-11 15:05:19 +0700

    Settings dishes's images

--- app/helpers/customers_helper.rb
+++
@@ -1,7 +0,0 @@
-module CustomersHelper
-  def gravatar_for user, size
-    gravatar_id = Digest::MD5.hexdigest user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag gravatar_url, class: "gravatar"
-  end
-end
